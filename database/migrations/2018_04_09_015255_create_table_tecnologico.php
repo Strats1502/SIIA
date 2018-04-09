d4bfe52d24e80dcd6bf48dbd0b3894b8427544cb
unknown <juanjoseesva@gmail.com>
2018-04-08 22:16:39 -0500

    first commit

---
+++ database/migrations/2018_04_09_015255_create_table_tecnologico.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableTecnologico extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('tecnologico', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('ruta_logo');
+            $table->string('direccion');
+
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('tecnologico');
+    }
+}
---
+++ database/migrations/2018_04_09_020130_create_table_administrador.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableAdministrador extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('administrador', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_usuario');
+
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('administrador');
+    }
+}
